682c3e2dae28878682d0fc8a1b813c63833fbcc5
sai-myne <ahlusir@gmail.com>
2020-08-06 15:14:22 +0800

    Introduction to Stores

--- src/App.svelte
+++ src/App.svelte
@@ -13,17 +13,7 @@
 		activeItem = e.detail;
 	}
 
-	// polls
-	let polls = [
-    {
-      id: 1,
-      question: 'Python or JavaScript?',
-      answerA: 'Python',
-      answerB: 'JavaScript',
-      votesA: 9,
-      votesB: 15,
-    },
-  ];
+	
 
   const handleAdd = e => {
 	  const poll = e.detail;
@@ -49,7 +39,7 @@
 <main>
 	<Tabs {activeItem} {items} on:tabChange={tabChange} />
 	{#if activeItem === 'Current Polls'}
-		<PollList polls={polls} on:vote={handleVote} />
+		<PollList on:vote={handleVote} />
 	{:else if activeItem === 'Add New Poll'}
 		<CreatePollForm on:add={handleAdd} />
 	{/if}
