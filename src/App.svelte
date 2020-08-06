6a25d37f4eb36f6033cdb8d981d89d12ef48e911
sai-myne <ahlusir@gmail.com>
2020-08-06 09:35:55 +0800

    Svelte Basics

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script>
-	export let name;
+	let name = 'Yoshi';
+	let beltColor = 'black';
+
+	const handleClick = () => {
+		beltColor = 'orange';
+	};
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Hello, Ninjas!</p>
+	<p>{beltColor} belt</p>
+	<button on:click={handleClick}>update belt colour</button>
 </main>
 
 <style>
