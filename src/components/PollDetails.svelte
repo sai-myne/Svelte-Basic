e1c22a67e4d6107fb3a280e8f20fd201af234a02
sai-myne <ahlusir@gmail.com>
2020-08-06 15:37:34 +0800

    Deleting Polls

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,6 +1,7 @@
 <script>
     import PollStore from '../stores/PollStore';
     import Card from '../shared/Card.svelte';
+    import Button from '../shared/Button.svelte'
     export let poll;
 
     // reactive values
@@ -22,6 +23,13 @@
         })
         
     };
+
+    // deleting a poll
+    const handleDelete = (id) => {
+        PollStore.update(currentPolls => {
+            return currentPolls.filter(poll => poll.id != id);
+        });
+    };
 </script>
 
 <Card>
@@ -35,6 +43,9 @@
         <div class="answer" on:click={() => handleVote('b', poll.id)}>
             <div class="percent percent-b" style="width: {percentB}%"></div>
             <span>{ poll.answerB } ({ poll.votesB }) </span>
+        </div>
+        <div class="delete">
+            <Button flat={true} on:click={() => handleDelete(poll.id)}>Delete</Button>
         </div>
     </div>
 </Card>
@@ -76,5 +87,9 @@ h3{
   .percent-b{
       border-left: 4px solid #45c496;
       background: rgba(69,196,150,0.2);
+  }
+  .delete{
+      margin-top: 30px;
+      text-align: center;
   }
 </style>
