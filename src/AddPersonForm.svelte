2ce56f5cb3cc4629de71df5ed9f86e10c4d2d1d5
sai-myne <ahlusir@gmail.com>
2020-08-06 12:17:59 +0800

    Using Forms part 2

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,9 +2,13 @@
     let name;
     let beltColour;
     let age;
+    // let fighting = false;
+    // let sneaking = false;
+    // let running = false;
+    let skills = [];
 
     const handleSubmit = () => {
-        console.log(name, beltColour, age);
+        console.log(name, beltColour, age, skills);
     }
 </script>
 
@@ -12,6 +16,17 @@
     <input type="text" placeholder="name" bind:value={name}>
     <input type="text" placeholder="belt colour" bind:value={beltColour}>
     <input type="number" bind:value={age} placeholder="age">
+    <label>Skills:</label>
+    <input type="checkbox" bind:group={skills} value="fighting">fighting<br >
+    <input type="checkbox" bind:group={skills} value="sneaking">sneaking<br >
+    <input type="checkbox" bind:group={skills} value="running">running<br >
+    <label>Belt Colour:</label>
+    <select bind:value={beltColour}>
+        <option value="black">black</option>
+        <option value="orange">orange</option>
+        <option value="brown">brown</option>
+        <option value="white">white</option>
+    </select>
     <button>Add Person</button>
 </form>
 
