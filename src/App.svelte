cae997aef54200baffc890dfb37e40b4d2d75856
sai-myne <ahlusir@gmail.com>
2020-08-06 10:17:53 +0800

    Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Modal from './Modal.svelte';
+
 	let people = [
     { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
     { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
@@ -13,14 +15,8 @@
   let num = 5;
 </script>
 
-{#if num > 20}
-	<p>Greater than 20</p>
-{:else if num > 5}
-	<p>Greater than 5</p>
-{:else}
-	<p>Not greater than 5</p>
-{/if}
 
+<Modal />
 <main>
 	<div>
 		{#each people as person (person.id)}
