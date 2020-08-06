c57337d2b83e2d21d2f52865eb95c02194b3f9b9
sai-myne <ahlusir@gmail.com>
2020-08-06 09:50:58 +0800

    Reactive Values

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,13 @@
 <script>
-	let name = 'Yoshi';
+	let firstName = 'Jimi';
+	let lastName = 'Hendrix';
 	let beltColor = 'black';
+
+	$: fullName = `${firstName} ${lastName}`;
+	$: {
+		console.log(beltColor);
+		console.log(fullName);
+	}
 
 	const handleClick = () => {
 		beltColor = 'orange';
@@ -12,10 +19,9 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p style="color: {beltColor}">{beltColor} belt</p>
-	<button on:click={handleClick}>update belt colour</button>
-	<!-- <input type="text" on:input={handleInput} value={beltColor}> -->
+	<p>{fullName} - {beltColor} belt</p>
+	<input type="text" bind:value={firstName}>
+	<input type="text" bind:value={lastName}>
 	<input type="text" bind:value={beltColor}>
 </main>
 
